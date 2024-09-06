8b2b7402c05c32e1299529cc2305f778c54c6e63
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-09-06 11:37:29 +0530

    Funny how JavaFX uses the stack-trace.
    Couldn't there have been a better way? Like making the method `protected` and `static`...?

--- src/com/brahvim/agc/server/App.java
+++ src/com/brahvim/agc/server/App.java
@@ -1,9 +1,13 @@
 package com.brahvim.agc.server;
 
+import javafx.application.Application;
+
 public class App {
 
     public static void main(final String[] p_args) {
-        JavaFxApp.launchApp();
+        Application.launch(JavaFxApp.class);
+        // PS Remember to *somehow get these arguments to the JVM for JavaFX:
+        // `--module-path ./lib/openjfx/ --add-modules javafx.controls,javafx.fxml`
     }
 
 }
