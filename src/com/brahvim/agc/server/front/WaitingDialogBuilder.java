2114bf4e3766906798b8de7c0e9dc3ff9c82aff5
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-09-06 19:47:47 +0530

    Two birds with one stone.
    - Fixed dialog not spawning perfectly in the middle,
    - Fixed dialog occasionally not spawning in the middle.

--- src/com/brahvim/agc/server/front/WaitingDialogBuilder.java
+++ src/com/brahvim/agc/server/front/WaitingDialogBuilder.java
@@ -1,5 +1,6 @@
 package com.brahvim.agc.server.front;
 
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
@@ -32,42 +33,47 @@ class WaitingDialogBuilder {
 		dialog.setResizable(false);
 		dialog.setHeight(144);
 		dialog.setWidth(720);
+		dialog.show();
 
-		// Would've positioned dialog at the center of the screen it *was* on, but...
-
-		// final double stageY = p_stage.getY();
-		// final double stageX = p_stage.getX();
-		//
-		// final Rectangle2D stageRect = new Rectangle2D(
-		//
-		// stageX,
-		// stageY,
-		// stageX + p_stage.getWidth(),
-		// stageY + p_stage.getHeight()
-		//
-		// );
-		//
-		// final var screens = Screen.getScreensForRectangle(stageRect);
-		//
-		// for (final Screen s : screens) {
-		// s.getBounds()
-		// }
-
+		// Would've positioned dialog at the center of the screen *it was most on*...
 		// ...But we don't really need that in this scenario, do we?:
+
+		// Add a listener to the dialog's width and height properties
+		final ChangeListener<? super Number> dimensionalChangesListener = //
+				(observableValue, oldVal, newVal) -> WaitingDialogBuilder.sendDialogToCenter(dialog);
+
+		dialog.widthProperty().addListener(dimensionalChangesListener);
+		dialog.heightProperty().addListener(dimensionalChangesListener);
+
+		// Setting the `x`-position before the `y`-position or using this combination of
+		// calls disallows the expected behavior:
+
+		// dialog.setWidth(720);
+		// dialog.setX((screenWidth - dialogWidth) / 2);
+		// dialog.setHeight(144);
+		// dialog.setY((screenHeight - dialogHeight) / 2);
+
+		// Platform.runLater(dialog::show);
+
+		// YES YES YES, this is *clearly* a race condition, and
+		// `WaitingDialogBuilder::sendDialogToCenter()` exists for this reason.
+		// Attach that guy to the listeners like I did above.
+
+		return dialog;
+	}
+
+	private static void sendDialogToCenter(final Dialog<?> p_dialog) {
 		final Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 
-		final double dialogWidth = p_stage.getWidth();
-		final double dialogHeight = p_stage.getHeight();
+		// I was using the `Stage` instance instead. Oops!:
+		final double dialogWidth = p_dialog.getWidth();
+		final double dialogHeight = p_dialog.getHeight();
 
 		final double screenWidth = screenBounds.getWidth();
 		final double screenHeight = screenBounds.getHeight();
 
-		dialog.setX((screenWidth - dialogWidth) / 2);
-		dialog.setY((screenHeight - dialogHeight) / 2);
-
-		dialog.show();
-
-		return dialog;
+		p_dialog.setX((screenWidth - dialogWidth) / 2);
+		p_dialog.setY((screenHeight - dialogHeight) / 2);
 	}
 
 	private static void addButtonsToPane(final DialogPane p_pane) {
