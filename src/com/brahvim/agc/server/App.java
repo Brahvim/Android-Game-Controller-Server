812d7d9f1a776fe8d39c9b00ed067402f23fa4f6
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-09-10 22:41:28 +0530

    So many clients!
    `IdentityHashMap`s cause a crash upon the creation of the 128th client. No idea why. Definitely not a hashing issue since `Integer::hashCode()` literally returns the underlying `int` itself - and I even *tested* with debug logs and checks for `null` keys and stuff. It wasn't broken for arbitrary indices - it just BROKE after `128`. Definitely *not* a hashing issue!
    
    `Integer::valueOf()`? `IntegerCache`? Bugs!!1!11!
    
    ...
    
    WAIT I DON'T NEED `Integer::Integer(int)` AT ALL! AUTOBOXING IS A THING! 🤦‍♂️
    
    But... WAIT, I have to box EVERY SINGLE ONE OF THEM now?! **EVERY SINGLE ONE!?!**

--- src/com/brahvim/agc/server/App.java
+++ src/com/brahvim/agc/server/App.java
@@ -1,5 +1,6 @@
 package com.brahvim.agc.server;
 
+import com.brahvim.agc.server.back.Client;
 import com.brahvim.agc.server.front.JavaFxApp;
 
 import javafx.application.Application;
@@ -14,6 +15,20 @@ public final class App {
 		// PS Remember to *somehow get these arguments to the JVM* for JavaFX:
 		// `--module-path ./lib/openjfx --add-modules javafx.controls,javafx.fxml`
 		// (I don't really need the `javafx.fxml` module for this app, but anyway.)
+
+		final int numClients = 50_000;
+		final Integer[] clients = new Integer[numClients];
+
+		for (int i = 0; i < numClients; ++i) {
+			System.out.println(i);
+			final Integer client = Client.createClient();
+			Client.setUdpSocketThread(client, Thread.currentThread());
+			System.out.println(Client.getUdpSocketThread(client).getName());
+			clients[i] = client;
+		}
+
+		for (int i = 0; i < numClients; ++i)
+			Client.destroyClient(clients[i]);
 
 		new Thread(null, App::launchFxApp, "AGC:FX_APP_LAUNCHER").start();
 	}
--- src/com/brahvim/agc/server/back/Client.java
+++ src/com/brahvim/agc/server/back/Client.java
@@ -16,7 +16,7 @@ public class Client {
 	private static final IdentityHashMap<Integer, Socket> socksSsl = new IdentityHashMap<>();
 
 	private static final ArrayDeque<Integer> freeIndices = new ArrayDeque<>();
-	private static final IdentityHashMap<Integer, Integer> idToIndexMap = new IdentityHashMap<>();
+	private static final IdentityHashMap<Integer, Integer> idToSoaIndexMap = new IdentityHashMap<>();
 
 	private static final AtomicBoolean inCreateOrDestroy = new AtomicBoolean();
 	// endregion
@@ -29,62 +29,101 @@ public class Client {
 		throw new IllegalAccessError();
 	}
 
-	public static synchronized int createClient() {
-		final var id = Client.freeIndices.poll();
+	public static synchronized Integer createClient() {
+		// synchronized (Client.waitForOtherCreateOrDestroy()) {
+		final Integer soaIndex = Client.idToSoaIndexMap.size();
+		Integer id = Client.freeIndices.poll();
 
-		final int minSize = Client.idToIndexMap.size();
-		Client.idToIndexMap.put(id, minSize);
-		Client.udpSockThreads.ensureCapacity(minSize);
-		Client.socksUdp.ensureCapacity(minSize);
+		if (id == null)
+			id = soaIndex;
 
-		Client.inCreateOrDestroy.set(false);
+		Client.idToSoaIndexMap.put(id, soaIndex);
+		Client.ensureArrayListSize(Client.socksUdp, soaIndex);
+		Client.ensureArrayListSize(Client.udpSockThreads, soaIndex);
+
+		// Client.endCurrentCreateOrDestroy();
 		return id;
+		// }
+
 	}
 
-	public static synchronized void destroyClient(final int p_clientId) {
+	public static synchronized void destroyClient(final Integer p_clientId) {
+		// synchronized (Client.waitForOtherCreateOrDestroy()) {
 		final var index = Client.mapIdToIndex(p_clientId);
 
 		// `Map`s:
 		Client.socksSsl.remove(index);
-		Client.idToIndexMap.remove(index);
+		Client.idToSoaIndexMap.remove(index);
 
 		// `List`s:
 		Client.socksUdp.set(index, null);
 		Client.udpSockThreads.set(index, null);
 
 		Client.freeIndices.add(p_clientId);
+		// Client.endCurrentCreateOrDestroy();
+		// }
 	}
 
 	// region Getters.
-	public static Socket getSslSocket(final int p_clientId) {
+	public static Socket getSslSocket(final Integer p_clientId) {
 		return Client.socksSsl.get(p_clientId);
 	}
 
-	public static Thread getUdpSocketThread(final int p_clientId) {
+	public static Thread getUdpSocketThread(final Integer p_clientId) {
 		return Client.udpSockThreads.get(Client.mapIdToIndex(p_clientId));
 	}
 
-	public static DatagramSocket getUdpSocket(final int p_clientId) {
+	public static DatagramSocket getUdpSocket(final Integer p_clientId) {
 		return Client.socksUdp.get(Client.mapIdToIndex(p_clientId));
 	}
 	// endregion
 
 	// region Setters.
-	public static synchronized void setSslSocket(final int p_clientId, final Socket p_sslSocket) {
-		Client.socksSsl.put(p_clientId, p_sslSocket);
+	public static synchronized Socket setSslSocket(final Integer p_clientId, final Socket p_sslSocket) {
+		return Client.socksSsl.put(p_clientId, p_sslSocket);
 	}
 
-	public static synchronized Thread setUdpSocketThread(final int p_clientId, final Thread p_thread) {
+	public static synchronized Thread setUdpSocketThread(final Integer p_clientId, final Thread p_thread) {
 		return Client.udpSockThreads.set(Client.mapIdToIndex(p_clientId), p_thread);
 	}
 
-	public static synchronized void setUdpSocket(final int p_clientId, final DatagramSocket p_udpSocket) {
-		Client.socksUdp.set(p_clientId, p_udpSocket);
+	public static synchronized DatagramSocket setUdpSocket(final Integer p_clientId, final DatagramSocket p_udpSocket) {
+		return Client.socksUdp.set(p_clientId, p_udpSocket);
 	}
 	// endregion
 
-	private static int mapIdToIndex(final int p_clientId) {
-		return Client.idToIndexMap.get(p_clientId);
+	private static void ensureArrayListSize(final ArrayList<?> p_list, final Integer p_minSize) {
+		// `Collection::addAll()`? Well, no!
+		// Putting my own `Collection` subclass didn't exactly work out, and `List.of()`
+		// won't create a `List` with `null`s! (See its use of `ImmutableCollections`!)
+		p_list.ensureCapacity(p_minSize);
+
+		while (p_list.size() <= p_minSize)
+			p_list.add(null);
+	}
+
+	private static AtomicBoolean waitForOtherCreateOrDestroy() {
+		synchronized (Client.inCreateOrDestroy) {
+			while (Client.inCreateOrDestroy.get())
+				try {
+					Client.inCreateOrDestroy.wait();
+				} catch (final InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+			return Client.inCreateOrDestroy;
+		}
+	}
+
+	private static AtomicBoolean endCurrentCreateOrDestroy() {
+		synchronized (Client.inCreateOrDestroy) {
+			Client.inCreateOrDestroy.set(false);
+			Client.inCreateOrDestroy.notifyAll();
+			return Client.inCreateOrDestroy;
+		}
+	}
+
+	private static Integer mapIdToIndex(final Integer p_clientId) {
+		return Client.idToSoaIndexMap.get(p_clientId);
 	}
 
 }
